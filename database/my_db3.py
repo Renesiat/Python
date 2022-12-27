e266f75207a5e61e44ae1bd614cf86c88c9433cc
Lin <Lin@DESKTOP-EOF2L9K>
2022-12-27 20:23:35 +0200

    cgi

--- database/my_db3.py
+++ database/my_db3.py
@@ -1,10 +1,10 @@
 # Робота з БД: таблиця Users
+import datetime
 import hashlib    # засоби гешування
 import mysql.connector
 import random
+import time 
 import uuid
-
-
 
 class User :
     def __init__( self, row = None ) -> None :
@@ -17,6 +17,7 @@ class User :
             self.avatar     = None
             self.email      = None
             self.email_code = None
+            self.del_dt     = None
         elif isinstance( row, dict ) :
             self.id         = row["id"]
             self.login      = row["login"]
@@ -26,6 +27,7 @@ class User :
             self.avatar     = row["avatar"]
             self.email      = row["email"]
             self.email_code = row["email_code"]
+            self.del_dt     = row["del_dt"]
         else :
             raise ValueError( "row type unsuppotred" )
 
@@ -74,7 +76,7 @@ class UserDAO :
             cursor.close()
         return
 
-    def read( self, id = None, login = None ) -> tuple | None :
+    def read( self, id = None, login = None, ignore_deleted = True ) -> tuple | None :
         sql = "SELECT u.* FROM `users` u "
         par = []
         if id :
@@ -83,11 +85,14 @@ class UserDAO :
         if login :
             sql += ( "AND" if id else "WHERE" ) + " u.`login` = %s "
             par.append( login )
+        if ignore_deleted :
+            sql += ( "AND" if id or login else "WHERE" ) + " u.`del_dt` IS NULL "
+
         try :
             cursor = self.db.cursor( dictionary = True )
             cursor.execute( sql, par )
         except mysql.connector.Error as err :
-            print( err )
+            print( 'read:', err )
         else :
             return tuple( User(row)  for row in cursor )
         finally :
@@ -95,40 +100,58 @@ class UserDAO :
         return None
 
     def read_auth( self, login, password ) -> User | None :
-        user = ( self.read( login = login ) + (None,) )[0]   # None - for empty list OR user
-        if user and self.hash_passw( password, user.salt ) == user.passw :
-            return user
+        user = ( self.read( login = login ) + (None,) )[0]
+        if user \
+            and user.del_dt == None \
+            and self.hash_passw( password, user.salt ) == user.passw :
+                return user
         return None
     
-    def read_auth_old( self, login, password ) -> User | None :
+    def update( self, user: User ) -> bool :
+        fields = user.__dict__.keys()
+        sql = "UPDATE `users` u SET " + \
+            ','.join( f"u.`{field.replace('passw', 'pass')}` = %({field})s"  for field in fields if field != 'id' ) + \
+            " WHERE u.`id` = %(id)s "
         try :
-            cursor = self.db.cursor( dictionary = True )
-            cursor.execute( "SELECT u.* FROM `users` u WHERE u.`login` = %s ", (login,) )
-            row = cursor.fetchone()
-            if row and self.hash_passw( password, row['salt'] ) == row['pass'] :
-                return User( row )
+            cursor = self.db.cursor()
+            cursor.execute( sql, user.__dict__ )
+            self.db.commit()
         except mysql.connector.Error as err :
-            print( 'read_auth:', err )
+            print( 'update', err )
+        else :
+            return True
+        finally :
+            cursor.close()
+        return False
+
+    def delete( self, user: User ) -> bool :
+        try :
+            cursor = self.db.cursor()
+            user.del_dt = time.strftime( '%Y-%m-%d %H:%M:%S', time.localtime() )
+            cursor.execute( "UPDATE users u SET u.del_dt = %s WHERE u.id = %s", (user.del_dt, user.id,) )
+            self.db.commit()            
+        except mysql.connector.Error as err :
+            print( 'delete', err )
+        else :
+            return True
         finally :
             try : cursor.close()
             except : pass
-        return None
-
+        return False
 
 
 def main( db_conf ) -> None :
-
     try :
         db = mysql.connector.connect( **db_conf )
     except mysql.connector.Error as err :
         print( err.errno, err )
         return
-    
+
     print( "Connection OK" )
     user = User()
-    user.login = "admin"
-    user.email = "admin@ukr.net"
-    user.name  = "Root Administrator"
+    user.login = "guest"
+    user.email = "guest@ukr.net"
+    user.name  = "Guest Walker"
     user.passw = "123"
     user.login = "user"
     user.email = "user@ukr.net"
@@ -137,16 +160,21 @@ def main( db_conf ) -> None :
     userDao = UserDAO( db )
     userDao.create( user )
     print( userDao.read() )
-    (user,) = userDao.read( id = 'd8df8963-0c16-4c9a-a85e-b6c35dfaa48a' ) + (None,) ; print( user )    
+    
     (user,) = userDao.read( id = '!d8df8963-0c16-4c9a-a85e-b6c35dfaa48a' ) + (None,) ; print( user )    
-    #(user,) = userDao.read( login = 'user' ) + (None,) ; print( user )    
+    (user,) = userDao.read( login = 'user' ) + (None,) ; print( user )    
     (user,) = userDao.read( login = '!user' ) + (None,) ; print( user )    
     print( (userDao.read( login = 'user' ) + (None,))[0] )
 
     print( userDao.read_auth( 'admin', '123' ) )
     print( userDao.read_auth( 'admin', '1234' ) )
     print( userDao.read_auth( 'odmin', '1234' ) )
-    print( userDao.read_auth( 'user', '123' ) )
+    ( user ) = userDao.read_auth( 'user', '123' )
+    ( user ) = userDao.read_auth( 'guest', '123' )
+    user.email = "user@meta.ua"
+    userDao.update( user )
+    userDao.delete( user )
+    print( user )
     return
 
 
@@ -163,26 +191,3 @@ if __name__ == "__main__" :
         "collation":   "utf8mb4_general_ci"
     }
     main( db_conf )
-
-# Організувати збереження конфігураційних даних (про підключення до БД)
-# у окремому файлі. Підключати файл у робочій програмі
-
-'''
-Скопіюємо таблицю (декларацію) з попереднього проєкту (Java)
-show create table users;
-CREATE TABLE `users` (
-  `id`     char(36)     NOT NULL DEFAULT uuid() COMMENT 'UUID',
-  `login`  varchar(64)  NOT NULL,
-  `pass`   char(40)     NOT NULL COMMENT 'SHA-160 hash',
-  `name`   varchar(128) NOT NULL,
-  `salt`   char(40)     DEFAULT NULL,
-  `avatar` varchar(64)  DEFAULT NULL COMMENT 'Avatar filename',
-  `email`  varchar(64)  DEFAULT NULL COMMENT 'User E-mail',
-  `email_code` char(6)  DEFAULT NULL COMMENT 'E-mail confirmation code',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-ORM : відображення даних на об'єкти  -  cтворення класів, структура
-яких відповідає структурі таблиць
-
-'''
